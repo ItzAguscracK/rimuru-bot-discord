15890b06d2f1814cd8e7f4de19616bddff7d8d9d
RimuruSenpai <66690766+ItzAguscracK@users.noreply.github.com>
2020-10-28 23:08:51 -0300

    Update Nusic

--- commands/musica/volume.js
+++ commands/musica/volume.js
@@ -13,9 +13,9 @@ module.exports = {
       if(!serverQueue) return message.channel.send('No hay canciones en la cola')
       if(!volume) return message.channel.send('Debes agregar un valor para el volumen')
 
-      if(volume < 2) return message.channel.send('La cantidad para el valora del volumen debe ser menor a 2')
+      if(volume > 2) return message.channel.send('La cantidad para el valora del volumen debe ser menor a 2')
 
-      if(volume > 0) return message.channel.send('La cantidad del valora para el volumen debe ser mayor a 0')
+      if(volume < 0) return message.channel.send('La cantidad del valora para el volumen debe ser mayor a 0')
       
       serverQueue.volume = volume;
       serverQueue.connection.dispatcher.setVolume(volume)
