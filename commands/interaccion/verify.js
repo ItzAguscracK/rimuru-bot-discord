4d93638d885b6e570a4a7d0df0c01047727b01e6
ItzAguscracK <trunkserver@gmail.com>
2020-11-12 17:35:34 -0300

    Update

--- commands/interaccion/verify.js
+++ commands/interaccion/verify.js
@@ -1,5 +1,8 @@
 module.exports = {
     name: "verify",
+    category: "interaccion",
+    description: "Verificacion en servidor oficial de Rimuru.",
+    usage: `verify`,
     run: async (client, message, args) => {
         message.delete()
         message.member.roles.add('753759760137977866')
