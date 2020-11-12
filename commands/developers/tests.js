ef1acd426637c7efb5d2262a15a634d00d91dd66
ItzAguscracK <trunkserver@gmail.com>
2020-11-12 15:32:10 -0300

    Update Rimuru

---
+++ commands/developers/tests.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "tests",
+  category: "developers",
+  description: "Este es un comando de pruebas, wow!.",
+  usage: `tests`,
+  run: async (client, message, args) => {
+
+    message.channel.send(':x: | Acceso denegado, unicamente desarrolladores!').then((m) => m.delete({ timeout: 2000 }));
+
+  },
+};
