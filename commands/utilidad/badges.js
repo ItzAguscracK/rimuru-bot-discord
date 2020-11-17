df49d89beada01dfcadafa07bc73ed61a2640913
ItzAguscracK <trunkserver@gmail.com>
2020-11-17 13:30:11 -0300

    test

--- commands/utilidad/badges.js
+++ commands/utilidad/badges.js
@@ -4,7 +4,7 @@ module.exports = {
   run: async (client, message, args) => {
 
     console.log('Alguien ha usado el comando badges');
-    /*
+    
     const users = message.mentions.users.first() || message.author  
 
     const variables = {
@@ -26,7 +26,7 @@ module.exports = {
     const embed = new MessageEmbed()
     .setDescription(users.flags.toArray().map(f=>variables[f]) ? users.flags.toArray().map(f=>variables[f]) : 'No tiene flags')
     return message.channel.send(embed)
-    */
+    
 
   },
 };
--- index.js
+++ index.js
@@ -1,43 +1,50 @@
-const { Client, Collection } = require('discord.js');
-const { config } = require('dotenv');
-const fs = require('fs');
-const mongoose = require('mongoose');
-const client = new Client({ ws: { properties: { $browser: "Discord Android" }}})
-const db   = 'mongodb+srv://db_user:99wanterronyes@cluster-db-discord.mbqup.gcp.mongodb.net/RimuruProject?' ||process.env.MONGODB_URI;
+const { Client, Collection, Intents } = require("discord.js");
+const { config } = require("dotenv");
+const fs = require("fs");
+const mongoose = require("mongoose");
+const client = new Client({
+  ws: {
+    intents: Intents.ALL,
+    properties: { $browser: "Discord Android" } },
+});
 
 client.commands = new Collection();
 client.aliases = new Collection();
-client.queue = new Map()
+client.queue = new Map();
 
-client.categories = fs.readdirSync('./commands/');
+client.categories = fs.readdirSync("./commands/");
 
-mongoose.set('useUnifiedTopology', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true)
+mongoose.set("useUnifiedTopology", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => {
     console.log(`DB connected`);
   })
-.catch(err => console.error(`Connection error ${err}`));
+  .catch((err) => console.error(`Connection error ${err}`));
 
 config({
-    path: `${__dirname}/.env`
+  path: `${__dirname}/.env`,
 });
 
-['command'].forEach(handler => {
-    require(`./handlers/${handler}`)(client);
+["command"].forEach((handler) => {
+  require(`./handlers/${handler}`)(client);
 });
 
-fs.readdir('./events/', (err, files) => {
-    if (err) return console.error;
-    files.forEach(file => {
-        if (!file.endsWith('.js')) return;
-        const evt = require(`./events/${file}`);
-        let evtName = file.split('.')[0];
-        console.log(`Eventos cargados '${evtName}'`);
-        client.on(evtName, evt.bind(null, client));
-    });
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error;
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    const evt = require(`./events/${file}`);
+    let evtName = file.split(".")[0];
+    console.log(`Eventos cargados '${evtName}'`);
+    client.on(evtName, evt.bind(null, client));
+  });
 });
 
 client.login(process.env.TOKEN);
+
+const db =
+  "mongodb+srv://db_user:99wanterronyes@cluster-db-discord.mbqup.gcp.mongodb.net/RimuruProject?" ||
+  process.env.MONGODB_URI;
